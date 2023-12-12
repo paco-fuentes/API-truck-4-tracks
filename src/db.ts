01782b5234c2c9326b89596f94461f44d5679125
Paco Fuentes <pacofuentes.work@gmail.com>
2023-12-12 19:30:09 +0100

    feat(compile): add script tocompile the app.js build

--- src/db.ts
+++ src/db.ts
@@ -1,18 +1,18 @@
 import "reflect-metadata";
-import 'dotenv/config';
+import "dotenv/config";
 import { DataSource } from "typeorm";
 
 type database = "mysql";
 
 export const AppDataSource = new DataSource({
-    type: process.env.DB_TYPE as database,
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT as string),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: [],
-    migrations: [],
-    synchronize: false,
-    logging: false,
-  });
+  type: process.env.DB_TYPE as database,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT as string),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [],
+  migrations: [],
+  synchronize: false,
+  logging: false,
+});
