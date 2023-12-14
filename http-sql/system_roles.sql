f5e42b5919c9b0cdffc2d38cf1248bc45f47ad4f
Paco Fuentes <pacofuentes.work@gmail.com>
2023-12-14 18:04:42 +0100

    feat(data): add sql data for system_roles and user activities tables

---
+++ http-sql/system_roles.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM `truck4tracks-db`.system_roles
+LIMIT 0, 1000
+
+-- Date: 2023-12-14 11:45
+*/
+INSERT INTO `system_roles` (`system_role`) VALUES ('user');
+INSERT INTO `system_roles` (`system_role`) VALUES ('admin');
+INSERT INTO `system_roles` (`system_role`) VALUES ('superAdmin');
