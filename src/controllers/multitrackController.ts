3537c0caaa5abeef3616d159bc699ccc3ee40d82
Paco Fuentes <pacofuentes.work@gmail.com>
2023-12-28 21:21:53 +0100

    feat(controllers/routes): add load track to multitrack

--- src/controllers/multitrackController.ts
+++ src/controllers/multitrackController.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from "express";
 import { BandMultitrack } from "../models/BandMultitrack";
 import { Band } from "../models/Band";
+import { BandMember } from "../models/BandMember";
+import { Track } from "../models/Track";
 
 const createMultitrack = async (req: Request, res: Response) => {
   try {
     const project_title = req.body.project_title;
     const img_url = req.body.img_url;
     const is_active = req.body.is_active;
+
+    console.log(req.params.id, req.token.id, req.body);
 
     // Usuario logueado
     const userId = req.token.id;
@@ -59,4 +63,59 @@ const createMultitrack = async (req: Request, res: Response) => {
   }
 };
 
-export { createMultitrack };
+const createTrack = async (req: Request, res: Response) => {
+  try {
+    const track_name = req.body.track_name;
+    const img_url = req.body.img_url;
+    const track_url = req.body.track_url;
+    const is_active = req.body.is_active;
+
+    // página de la banda
+    const bandId = parseInt(req.params.id);
+    // usuario logueado
+    const userId = req.token.id;
+    // id del multitrack actual
+    const multitrackId = req.body.id;
+    // encuentra el multitrack
+    // const multitrack = await BandMultitrack.findBy({
+    //   id: multitrackId,
+    // });
+
+    const bandMember = await BandMember.find({
+      where: { band_id: bandId, user_id: userId },
+    });
+
+    if (!bandMember) {
+      return res.json({
+        success: false,
+        message: bandMember
+          ? "User is band member"
+          : "User is not band member",
+      });
+    }
+
+    const newTrack = await Track.create({
+      multitrack_id: multitrackId,
+      user_id: userId,
+      track_name: track_name,
+      img_url: img_url,
+      track_url: track_url,
+      is_active: is_active,
+    }).save();
+    console.log(newTrack);
+
+    return res.json({
+      success: true,
+      message: "Track loaded successfully",
+      data: newTrack,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Failed to load the track",
+      error: error,
+    });
+  }
+};
+
+export { createMultitrack, createTrack };
--- src/routes/multitrackRoutes.ts
+++ src/routes/multitrackRoutes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { auth } from "../middleware/auth";
 import { isUser } from "../middleware/isUser";
-import { createMultitrack } from "../controllers/multitrackController";
+import { createMultitrack, createTrack } from "../controllers/multitrackController";
 
 const router = Router();
 
 // create multitrack as band leader
 router.post("/create/:id", auth, isUser, createMultitrack)
+// load a track to multitrack
+router.post("/loadtrack/:id", auth, isUser, createTrack)
 
 export { router };
